37907722fbf10ba55a0221de3d8c86e8f031beef
Teten Nugraha <teten.nugraha18@gmail.com>
2022-02-14 11:58:36 +0700

    preparation auth

--- handler/authHandler.go
+++ handler/authHandler.go
@@ -1,7 +1,19 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/teten-nugraha/go-admin/model"
+)
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello World")
+func Register(c *fiber.Ctx) error {
+
+	user := model.User{
+		FirstName: "Teten",
+	}
+
+	user.LastName = "NUgraha"
+	user.Email = "teten.nugraha18@gmail.com"
+	user.Password = "Secret"
+
+	return c.JSON(user)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,5 +6,5 @@ import (
 )
 
 func SetupRoutes(app *fiber.App) {
-	app.Get("/", handler.Hello)
+	app.Get("/", handler.Register)
 }
